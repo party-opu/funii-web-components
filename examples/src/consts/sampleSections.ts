0a167cbac485b2f79bae395385929fc2f66be62c
kosaku kurino <kousaku.maron@gmail.com>
2020-11-12 16:09:16 +0900

    feat: update example page

---
+++ examples/src/consts/sampleSections.ts
@@ -0,0 +1,42 @@
+export const carouselSections = [
+  {
+    fields: {
+      imageURL: {
+        label: '画像',
+        order: 10,
+        type: 'image',
+        value: 'https://www.nintendo.co.jp/switch/ampna/material/img/top/mv.jpg',
+      },
+    },
+  },
+  {
+    fields: {
+      imageURL: {
+        label: '画像',
+        order: 10,
+        type: 'image',
+        value: 'https://www.nintendo.co.jp/switch/ampna/material/img/top/bg_pikmin.jpg',
+      },
+    },
+  },
+]
+
+export const missionSections = [
+  {
+    fields: {
+      title: {
+        label: 'タイトル',
+        order: 10,
+        type: 'text',
+        value: '素敵な出会いを紡ぐ',
+      },
+      description: {
+        label: '詳細',
+        order: 10,
+        type: 'text',
+        value:
+          '生きている間に何人の人と出会えるだろうか？\n人との出会いは未知数で何が起きるかわからない。\nfuniiは起こりうる未知数を増やすことで、素敵な出会いで溢れる世界を作ります。',
+      },
+    },
+  },
+]
