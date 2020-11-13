e1adcdcf3c13073e06bd3fe662626b1984a2a4f6
kosaku kurino <kousaku.maron@gmail.com>
2020-11-14 00:30:52 +0900

    feat: app other component to welcome page

--- examples/src/consts/sampleSections.ts
+++ examples/src/consts/sampleSections.ts
@@ -40,3 +40,193 @@ export const missionSections = [
     },
   },
 ]
+
+export const serviceSections = [
+  {
+    fields: {
+      imageURL: {
+        label: '画像',
+        order: 10,
+        type: 'text',
+        value: 'https://www.nintendo.co.jp/switch/ampna/material/img/top/bg_pikmin.jpg',
+      },
+      title: {
+        label: 'タイトル',
+        order: 20,
+        type: 'text',
+        value: 'モリミン３',
+      },
+      description: {
+        label: '詳細',
+        order: 30,
+        type: 'text',
+        value:
+          '生きている間に何人の人と出会えるだろうか？\n人との出会いは未知数で何が起きるかわからない。\nfuniiは起こりうる未知数を増やすことで、素敵な出会いで溢れる世界を作ります。',
+      },
+    },
+  },
+  {
+    fields: {
+      imageURL: {
+        label: '画像',
+        order: 10,
+        type: 'text',
+        value: 'https://www.nintendo.co.jp/switch/ampna/material/img/top/bg_pikmin.jpg',
+      },
+      title: {
+        label: 'タイトル',
+        order: 20,
+        type: 'text',
+        value: 'モリミン３',
+      },
+      description: {
+        label: '詳細',
+        order: 30,
+        type: 'text',
+        value:
+          '生きている間に何人の人と出会えるだろうか？\n人との出会いは未知数で何が起きるかわからない。\nfuniiは起こりうる未知数を増やすことで、素敵な出会いで溢れる世界を作ります。',
+      },
+    },
+  },
+  {
+    fields: {
+      imageURL: {
+        label: '画像',
+        order: 10,
+        type: 'text',
+        value: 'https://www.nintendo.co.jp/switch/ampna/material/img/top/bg_pikmin.jpg',
+      },
+      title: {
+        label: 'タイトル',
+        order: 20,
+        type: 'text',
+        value: 'モリミン３',
+      },
+      description: {
+        label: '詳細',
+        order: 30,
+        type: 'text',
+        value:
+          '生きている間に何人の人と出会えるだろうか？\n人との出会いは未知数で何が起きるかわからない。\nfuniiは起こりうる未知数を増やすことで、素敵な出会いで溢れる世界を作ります。',
+      },
+    },
+  },
+]
+
+export const companySections = [
+  {
+    fields: {
+      title: {
+        label: '会社名',
+        order: 10,
+        type: 'text',
+        value: 'funii inc',
+      },
+      ceoName: {
+        label: '代表者',
+        order: 20,
+        type: 'text',
+        value: '栗野　耕作',
+      },
+      foundationDate: {
+        label: '設　立',
+        order: 30,
+        type: 'text',
+        value: '2020年10月1日',
+      },
+      address: {
+        label: '住　所',
+        order: 40,
+        type: 'text',
+        value: '〒103-0026 東京都中央区日本橋兜町18-5 日本橋兜町ビル 2F',
+      },
+    },
+  },
+]
+
+export const memberSections = [
+  {
+    fields: {
+      imageURL: {
+        label: '画像',
+        order: 10,
+        type: 'image',
+        value: 'https://www.nintendo.co.jp/switch/ampna/material/img/prologue/img_chara2.png',
+      },
+      name: {
+        label: '名前',
+        order: 20,
+        type: 'text',
+        value: 'ジョン',
+      },
+      role: {
+        label: '肩書き',
+        order: 30,
+        type: 'text',
+        value: 'Fonder CEO',
+      },
+      description: {
+        label: 'プロフィール',
+        order: 40,
+        type: 'text',
+        value: '2020年に東京へ上京し、株式会社モリミンを設立する。',
+      },
+    },
+  },
+  {
+    fields: {
+      imageURL: {
+        label: '画像',
+        order: 10,
+        type: 'image',
+        value: 'https://material-ui.com/static/images/avatar/1.jpg',
+      },
+      name: {
+        label: '名前',
+        order: 20,
+        type: 'text',
+        value: 'ジョン',
+      },
+      role: {
+        label: '肩書き',
+        order: 30,
+        type: 'text',
+        value: 'Fonder CEO',
+      },
+      description: {
+        label: 'プロフィール',
+        order: 40,
+        type: 'text',
+        value: '2020年に東京へ上京し、株式会社モリミンを設立する。',
+      },
+    },
+  },
+  {
+    fields: {
+      imageURL: {
+        label: '画像',
+        order: 10,
+        type: 'image',
+        value: 'https://material-ui.com/static/images/avatar/1.jpg',
+      },
+      name: {
+        label: '名前',
+        order: 20,
+        type: 'text',
+        value: 'ジョン',
+      },
+      role: {
+        label: '肩書き',
+        order: 30,
+        type: 'text',
+        value: 'Fonder CEO',
+      },
+      description: {
+        label: 'プロフィール',
+        order: 40,
+        type: 'text',
+        value: '2020年に東京へ上京し、株式会社モリミンを設立する。',
+      },
+    },
+  },
+]
