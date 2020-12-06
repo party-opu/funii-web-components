ebaa653571b50c5d43656dd9f908ffea70c7b2ba
kosaku kurino <kousaku.maron@gmail.com>
2020-12-06 20:34:34 +0900

    feat: add header and footer

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,115 @@
+import React from 'react'
+import styled from 'styled-components'
+import Spacer from '../../core/spacer'
+import { ComponentProps } from '../props'
+import { useRouting, useExistLink } from '../hooks'
+
+// TODO: 会社名(copylight)とロゴをどうするか考えないといけない。
+const Footer = ({ sections, push, paths = [] }: ComponentProps) => {
+  const onClick = useRouting(push)
+  const onCheckExistLink = useExistLink()
+
+  return (
+    <Root>
+      <Nav>
+        {/* <Spacer />
+        <Logo src="/funii.svg" />
+        <Spacer size="l" /> */}
+        <ListWrapper>
+          <List>
+            {sections.map((section, index) => (
+              <React.Fragment key={index}>
+                <Spacer layout="vertical" size="l" />
+                <ListItem data-existlink={onCheckExistLink(section.fields.text, paths)} onClick={() => onClick(section.fields.text, paths)}>
+                  <ListItemText>{section.fields.text.value}</ListItemText>
+                </ListItem>
+                <Spacer layout="vertical" size="l" />
+              </React.Fragment>
+            ))}
+          </List>
+        </ListWrapper>
+        <Spacer size="xxl" />
+        <CopyrightText>Copyright ©︎ 2020 xxx inc. All Rights Reserved.</CopyrightText>
+        <Spacer size="l" />
+      </Nav>
+    </Root>
+  )
+}
+
+const Root = styled.footer`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`
+
+const Nav = styled.nav`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 1080px;
+`
+
+// const Logo = styled.img`
+//   height: 80px;
+
+//   @media (min-width: 400px) {
+//     height: 120px;
+//   }
+// `
+
+const ListWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  width: 100%;
+`
+
+const List = styled.ul`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  width: 100%;
+`
+
+const ListItem = styled.li``
+
+const ListItemText = styled.p`
+  text-decoration: none;
+  font-weight: bold;
+  font-size: 9px;
+  color: ${(props) => props.theme.foregrounds.primary};
+
+  @media (min-width: 400px) {
+    font-size: 12px;
+  }
+`
+
+ListItemText.defaultProps = {
+  theme: {
+    foregrounds: {
+      primary: '#404040',
+    },
+  },
+}
+
+const CopyrightText = styled.p`
+  font-weight: bold;
+  font-size: 9px;
+  color: ${(props) => props.theme.foregrounds.primary};
+  text-align: center;
+
+  @media (min-width: 400px) {
+    font-size: 12px;
+  }
+`
+
+CopyrightText.defaultProps = {
+  theme: {
+    foregrounds: {
+      primary: '#404040',
+    },
+  },
+}
+
+export default Footer
