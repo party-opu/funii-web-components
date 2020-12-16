e89464ff9ce80b14bfbfaaba77d94fb686b9ccd7
tabikaeru <moriokayuixxx@gmail.com>
2020-12-17 04:39:28 +0900

    fix:Focentelize_Footer_position

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -31,7 +31,7 @@ const Footer = ({ sections, push, paths = [] }: ComponentProps) => {
 
 const Root = styled.footer`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   align-items: center;
   width: 100%;
   height: 60px;
