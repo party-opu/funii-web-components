f53bfcc89fcf469b53e5c5505b7c05c8293485b0
kosaku kurino <kousaku.maron@gmail.com>
2021-02-12 13:07:52 +0900

    fix: render tree view

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { ComponentProps, Frame as FrameNode } from './props'
 import Frame, { FrameItemWrapper } from './frame'
 import Text from './text'
@@ -13,66 +13,70 @@ import Member from './member'
 import Contact from './contact'
 
 const Component = ({ node, push, paths, preview = false, onSend }: ComponentProps) => {
-  switch (node.type) {
-    // Layouts
-    // -----------------------------
-    case 'frame': {
-      const frame = node as FrameNode
-      return (
-        <Frame node={frame}>
-          {frame.children.map((childNode) => {
-            return (
-              <FrameItemWrapper key={childNode.id}>
-                <Component node={node} push={push} paths={paths} preview={preview} />
-              </FrameItemWrapper>
-            )
-          })}
-        </Frame>
-      )
-    }
+  const NodeTree = useCallback(({ node, push, paths, preview, onSend }: ComponentProps) => {
+    switch (node.type) {
+      // Layouts
+      // -----------------------------
+      case 'frame': {
+        const frame = node as FrameNode
+        return (
+          <Frame node={frame}>
+            {frame.children.map((childNode) => {
+              return (
+                <FrameItemWrapper key={childNode.id}>
+                  <NodeTree node={childNode} push={push} paths={paths} preview={preview} onSend={onSend} />
+                </FrameItemWrapper>
+              )
+            })}
+          </Frame>
+        )
+      }
 
-    // BasicNodes
-    // -----------------------------
-    case 'text': {
-      return <Text node={node} push={push} paths={paths} preview={preview} />
-    }
-    case 'image': {
-      return <Image node={node} push={push} paths={paths} preview={preview} />
-    }
+      // BasicNodes
+      // -----------------------------
+      case 'text': {
+        return <Text node={node} push={push} paths={paths} preview={preview} />
+      }
+      case 'image': {
+        return <Image node={node} push={push} paths={paths} preview={preview} />
+      }
 
-    // ComponentSets
-    // -----------------------------
-    case 'header': {
-      return <Header node={node} push={push} paths={paths} preview={preview} />
+      // ComponentSets
+      // -----------------------------
+      case 'header': {
+        return <Header node={node} push={push} paths={paths} preview={preview} />
+      }
+      case 'footer': {
+        return <Footer node={node} push={push} paths={paths} preview={preview} />
+      }
+      case 'carousel': {
+        return <Carousel node={node} push={push} paths={paths} preview={preview} />
+      }
+      case 'mission': {
+        return <Mission node={node} push={push} paths={paths} preview={preview} />
+      }
+      case 'service': {
+        return <Service node={node} push={push} paths={paths} preview={preview} />
+      }
+      case 'table': {
+        return <Table node={node} push={push} paths={paths} preview={preview} />
+      }
+      case 'member': {
+        return <Member node={node} push={push} paths={paths} preview={preview} />
+      }
+      case 'contact': {
+        return <Contact node={node} preview={preview} onSend={onSend} />
+      }
+      case 'space': {
+        return <Text node={node} push={push} paths={paths} preview={preview} />
+      }
+      default: {
+        return null
+      }
     }
-    case 'footer': {
-      return <Footer node={node} push={push} paths={paths} preview={preview} />
-    }
-    case 'carousel': {
-      return <Carousel node={node} push={push} paths={paths} preview={preview} />
-    }
-    case 'mission': {
-      return <Mission node={node} push={push} paths={paths} preview={preview} />
-    }
-    case 'service': {
-      return <Service node={node} push={push} paths={paths} preview={preview} />
-    }
-    case 'table': {
-      return <Table node={node} push={push} paths={paths} preview={preview} />
-    }
-    case 'member': {
-      return <Member node={node} push={push} paths={paths} preview={preview} />
-    }
-    case 'contact': {
-      return <Contact node={node} preview={preview} onSend={onSend} />
-    }
-    case 'space': {
-      return <Text node={node} push={push} paths={paths} preview={preview} />
-    }
-    default: {
-      return null
-    }
-  }
+  }, [])
+
+  return <NodeTree node={node} push={push} paths={paths} preview={preview} onSend={onSend} />
 }
 
 export default Component
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import Component from './components'
+import Frame, { FrameItemWrapper } from './components/frame'
 import Text from './components/text'
 import Image from './components/image'
 import Carousel from './components/carousel'
@@ -7,6 +8,8 @@ import Table from './components/table'
 import Member from './components/member'
 import Contact from './components/contact'
 
-export { Text, Image, Carousel, Mission, Table, Member, Contact }
+export { Frame, FrameItemWrapper }
+export { Text, Image }
+export { Carousel, Mission, Table, Member, Contact }
 
 export default Component
