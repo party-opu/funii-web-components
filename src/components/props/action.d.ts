81909395255a6727c9008df10db5ade8473969f6
kousaku-maron <kousaku-maron@users.noreply.github.com>
2021-03-17 03:06:46 +0000

    deploy: 1fc7269c465dc62dad4e64ebab7f409d8a97e964

--- src/components/props/action.d.ts
+++ src/components/props/action.d.ts
@@ -1,6 +1,20 @@
+declare type FlexibleString = {
+    type: 'text' | 'variable';
+    value: string;
+}[];
+declare type Variable = {
+    key: string;
+    value: string;
+};
 interface BaseAction {
     trigger: 'click';
     type: 'externalLink' | 'internalLink' | 'api';
+    input: {
+        variables: Variable[];
+    };
+    output: {
+        variables: Variable[];
+    };
 }
 interface LinkAction extends BaseAction {
     type: 'externalLink' | 'internalLink';
@@ -10,17 +24,17 @@ interface BaseAPIAction extends BaseAction {
     type: 'api';
     method: 'get' | 'post';
     endpoint: `http://${string}` | `https://${string}`;
-    authorization: string | null;
+    authorization: FlexibleString;
     headers: {
         key: string;
-        value: string;
+        value: FlexibleString;
     }[];
 }
 interface GetAPIAction extends BaseAPIAction {
     method: 'get';
     queryParams: {
         key: string;
-        value: string;
+        value: FlexibleString;
     }[];
 }
 interface PostAPIAction extends BaseAPIAction {
@@ -28,7 +42,7 @@ interface PostAPIAction extends BaseAPIAction {
     payloadType: 'form' | 'json' | 'xml' | 'row';
     data: {
         key: string;
-        value: string;
+        value: FlexibleString;
     }[];
     file: any;
 }
